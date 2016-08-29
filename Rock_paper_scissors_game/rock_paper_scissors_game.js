b14e5d5489ab4a30a60416e5e0818de06246ced6
Zach Stradling <zachstradling@gmail.com>
2016-08-29 15:20:43 -0700

    updated script src from test.js to rock_paper_scissors_game.js

--- Rock_paper_scissors_game/rock_paper_scissors_game.js
+++ Rock_paper_scissors_game/rock_paper_scissors_game.js
@@ -1,9 +1,13 @@
 
-/*
-var test = prompt("what's up?");
 
 
 
+
+
+
+var userChoice = function (selection) { 
+if (selection === "rock") {
+    document.getElementById('user_answer').innerHTML = "You threw " + selection + "!";
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
     computerChoice = "rock";
@@ -11,61 +15,12 @@ if (computerChoice < 0.34) {
     computerChoice = "paper";
 } else {
     computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
+} document.getElementById('computer_answer').innerHTML = "I went with " + computerChoice + "!";
 
+compare(selection, computerChoice);
 
-
-function userChoice("selection") {
-
-if (selection === "rock") {
-    compare(selection, computerChoice);
-    alert("hello");
 } else if (selection === "scissors") {
-    compare(selection, computerChoice);
-} else if (selection === "paper") {
-    compare(selection, computerChoice);
-}
-}
-
-function compare(choice1, choice2) {
-    
-    if (choice1 === choice2) {
-        
-        alert("The result is a tie!");
-        } else if (choice1 === "rock") {
-            if (choice2 === "scissors") {
-                alert("rock wins") ;
-                } else {
-                alert("paper wins") ;    
-                    }
-            } else if (choice1 === "paper") {
-            if (choice2 === "rock") {
-                alert("paper wins");
-                } else {
-                alert("scissors wins");    
-                    }
-            } else if (choice1 === "scissors") {
-            if (choice2 === "rock") {
-                alert("rock wins");
-                } else {
-                alert("scissors wins");    
-                    }
-        } else if (choice1 != "rock" || choice1 != "scissors" || choice1 != "paper" ) {                     
-            alert("You did not enter a valid answer.");
-            }
-    }
-//compare(userChoice, computerChoice);
-
-
-//var userChoice = prompt("Do you choose rock, paper or scissors?");
-
-
-
-
-
-
-
-/*
+    document.getElementById('user_answer').innerHTML = "You threw " + selection + "!";
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
     computerChoice = "rock";
@@ -73,46 +28,63 @@ if (computerChoice < 0.34) {
     computerChoice = "paper";
 } else {
     computerChoice = "scissors";
-} //console.log("Computer: " + computerChoice);
+} document.getElementById('computer_answer').innerHTML = "I went with " + computerChoice + "!";
 
+compare(selection, computerChoice);
 
-
-function userChoice(selection) {
-
-if (selection === "rock") {
-    compare(selection, computerChoice);
-    alert("hello");
-} else if (selection === "scissors") {
-    compare(selection, computerChoice);
 } else if (selection === "paper") {
-    compare(selection, computerChoice);
+     document.getElementById('user_answer').innerHTML = "You threw " + selection + "!";
+    var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+} document.getElementById('computer_answer').innerHTML = "I went with " + computerChoice + "!";
+
+compare(selection, computerChoice);
+
 }
-}
+};
 
 function compare(choice1, choice2) {
     
     if (choice1 === choice2) {
         
-        alert("The result is a tie!");
+        document.getElementById('who_won').innerHTML = "It's a tie!";
         } else if (choice1 === "rock") {
             if (choice2 === "scissors") {
-                alert("rock wins") ;
+                
+                document.getElementById('who_won').innerHTML = "Rock Wins!";
+                
                 } else {
-                alert("paper wins") ;    
+                
+                  document.getElementById('who_won').innerHTML = "Paper Wins!";
+                  
                     }
             } else if (choice1 === "paper") {
             if (choice2 === "rock") {
-                alert("paper wins");
+                
+                document.getElementById('who_won').innerHTML = "Paper Wins!";
+                
                 } else {
-                alert("scissors wins");    
+                
+                document.getElementById('who_won').innerHTML = "Scissors Wins!";
+                  
                     }
             } else if (choice1 === "scissors") {
             if (choice2 === "rock") {
-                alert("rock wins");
+                
+                document.getElementById('who_won').innerHTML = "Rock Wins!";
+                
                 } else {
-                alert("scissors wins");    
+                
+                document.getElementById('who_won').innerHTML = "Scissors Wins!";
+                    
                     }
         } else if (choice1 != "rock" || choice1 != "scissors" || choice1 != "paper" ) {                     
             alert("You did not enter a valid answer.");
             }
     }
+
